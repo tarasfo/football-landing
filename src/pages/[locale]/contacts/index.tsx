536f80ca24efdb5a15cc5ec1e5aa23993e1dc69d
Albert <rudenko.albert.n@gmail.com>
2023-06-27 20:56:24 +0300

    empty pages error fixed

--- src/pages/[locale]/contacts/index.tsx
+++ src/pages/[locale]/contacts/index.tsx
@@ -1,111 +1,105 @@
-// import Head from "next/head";
-// import { Button } from "@material-ui/core";
-// import { Box, Typography } from "@mui/material";
-// import { InferGetStaticPropsType } from "next";
-// import { createStaticTerm } from "next-g11n";
-// import Image from "next/image";
-// import {
-//   AiOutlineCalendar,
-//   AiOutlineMail,
-//   AiOutlinePhone,
-// } from "react-icons/ai";
+import Head from "next/head";
+import { Button } from "@material-ui/core";
+import { Box, Typography } from "@mui/material";
+import { InferGetStaticPropsType } from "next";
+import { createStaticTerm } from "next-g11n";
+import Image from "next/image";
+import {
+  AiOutlineCalendar,
+  AiOutlineMail,
+  AiOutlinePhone,
+} from "react-icons/ai";
 
-// import Footer from "@/components/footer";
-// import Header from "@/components/header";
-// import styles from "./Contact.module.css";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import styles from "./Contact.module.css";
 
-// export default function Privacy({
-//   footerSectionKeys,
-//   header,
-//   download,
-//   calendly,
-//   contacts,
-// }: InferGetStaticPropsType<typeof getStaticProps>) {
-//   return (
-//     <>
-//       <Head>
-//         <title>Digitalize</title>
-//         <meta name="description" content="Lending page for football app" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-//       <main className={styles.mainContainer}>
-//         <Header header={header} download={download} calendly={calendly} />
-//         <Box className={styles.bgColor}>
-//           <Box className={styles.widthHalf}>
-//             <Box className={styles.flexRow}>
-//               <Box className={styles.line}></Box>
-//               <Typography className={styles.typographyUppercase}>
-//                 Напишіть нашій команді, щоб дізнатись як ми можемо допомогти
-//               </Typography>
-//             </Box>
-//             <Typography className={styles.typographyNormal}>
-//               Нам треба всього пара годин!
-//             </Typography>
+export default function Privacy() {
+  return (
+    <>
+      <Head>
+        <title>Digitalize</title>
+        <meta name="description" content="Lending page for football app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.mainContainer}>
+        {/* <Header header={header} download={download} calendly={calendly} />
+        <Box className={styles.bgColor}>
+          <Box className={styles.widthHalf}>
+            <Box className={styles.flexRow}>
+              <Box className={styles.line}></Box>
+              <Typography className={styles.typographyUppercase}>
+                Напишіть нашій команді, щоб дізнатись як ми можемо допомогти
+              </Typography>
+            </Box>
+            <Typography className={styles.typographyNormal}>
+              Нам треба всього пара годин!
+            </Typography>
 
-//             <Image src="/branding.png" width="423" alt="Feature" height={423} />
-//           </Box>
-//           <Box className={styles.flexColumn}>
-//             <Box className={styles.flexRowCenter}>
-//               <AiOutlinePhone
-//                 width={30}
-//                 className={styles.iconStyle}
-//                 color="#21BF73"
-//               />
-//               <Typography className={styles.typographyMedium}>
-//                 Телефон
-//               </Typography>
-//             </Box>
-//             <Typography>
-//               <a
-//                 style={{
-//                   textDecoration: "none",
-//                   fontSize: "1.5rem",
-//                 }}
-//                 href={`tel:${contacts.phone}`}
-//               >
-//                 {contacts.phone}
-//               </a>
-//             </Typography>
+            <Image src="/branding.png" width="423" alt="Feature" height={423} />
+          </Box>
+          <Box className={styles.flexColumn}>
+            <Box className={styles.flexRowCenter}>
+              <AiOutlinePhone
+                width={30}
+                className={styles.iconStyle}
+                color="#21BF73"
+              />
+              <Typography className={styles.typographyMedium}>
+                Телефон
+              </Typography>
+            </Box>
+            <Typography>
+              <a
+                style={{
+                  textDecoration: "none",
+                  fontSize: "1.5rem",
+                }}
+                href={`tel:${contacts.phone}`}
+              >
+                {contacts.phone}
+              </a>
+            </Typography>
 
-//             <Box className={styles.flexRowCenter}>
-//               <AiOutlineMail
-//                 className={styles.iconStyle}
-//                 width={30}
-//                 color="#21BF73"
-//               />
-//               <Typography className={styles.typographyMedium}>Пошта</Typography>
-//             </Box>
-//             <a className={styles.emailButton} href={`mailto:${contacts.email}`}>
-//               Написати
-//             </a>
+            <Box className={styles.flexRowCenter}>
+              <AiOutlineMail
+                className={styles.iconStyle}
+                width={30}
+                color="#21BF73"
+              />
+              <Typography className={styles.typographyMedium}>Пошта</Typography>
+            </Box>
+            <a className={styles.emailButton} href={`mailto:${contacts.email}`}>
+              Написати
+            </a>
 
-//             <Box className={styles.flexRowCenter}>
-//               <AiOutlineCalendar
-//                 width={30}
-//                 className={styles.iconStyle}
-//                 color="#21BF73"
-//               />
-//               <Typography className={styles.typographyMedium}>
-//                 Calendly
-//               </Typography>
-//             </Box>
-//             <Button className={styles.calendlyButton}>
-//               <a
-//                 style={{ textDecoration: "none", color: "white" }}
-//                 href={`${contacts.calendly}`}
-//                 target="_blank"
-//               >
-//                 Запланувати дзвінок
-//               </a>
-//             </Button>
-//           </Box>
-//         </Box>
-//         <Footer footerSectionKeys={footerSectionKeys} />
-//       </main>
-//     </>
-//   );
-// }
+            <Box className={styles.flexRowCenter}>
+              <AiOutlineCalendar
+                width={30}
+                className={styles.iconStyle}
+                color="#21BF73"
+              />
+              <Typography className={styles.typographyMedium}>
+                Calendly
+              </Typography>
+            </Box>
+            <Button className={styles.calendlyButton}>
+              <a
+                style={{ textDecoration: "none", color: "white" }}
+                href={`${contacts.calendly}`}
+                target="_blank"
+              >
+                Запланувати дзвінок
+              </a>
+            </Button>
+          </Box>
+        </Box>
+        <Footer footerSectionKeys={footerSectionKeys} /> */}
+      </main>
+    </>
+  );
+}
 
 // import fs from "fs";
 // import path from "path";
