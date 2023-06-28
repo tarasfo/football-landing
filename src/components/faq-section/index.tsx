1d4b309b5a087079de118ffa19ba806746da20a7
Albert <rudenko.albert.n@gmail.com>
2023-06-28 11:45:33 +0300

    linl error fixed

--- src/components/faq-section/index.tsx
+++ src/components/faq-section/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import style from "./FAQ.module.css";
 import FAQAccordion from "./components";
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
 
 function FAQSection({ data }: { data: any }) {
   const { t, i18n } = useTranslation("common");
@@ -13,9 +14,9 @@ function FAQSection({ data }: { data: any }) {
       <Box className={style.headingContainer}>
         <h2 className={style.mainHeading}>{t("faq-title")}</h2>
         <div className={style.additionalInfo}>{t("questions")}</div>
-        <a className={style.contactLink} href="/contacts">
+        <Link className={style.contactLink} href="/contacts">
           {t("contact")}
-        </a>
+        </Link>
       </Box>
       <Box className={style.accordionGrid}>
         {data.map((item: any, index: number) => {
