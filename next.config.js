54477c139263f9ce224397339a7fd57def60bde3
Albert <rudenko.albert.n@gmail.com>
2023-06-28 12:27:03 +0300

    redirects added

--- next.config.js
+++ next.config.js
@@ -6,6 +6,50 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/en",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/ua",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/en/contacts",
+        destination: "/contacts",
+        permanent: true,
+      },
+      {
+        source: "/ua/contacts",
+        destination: "/contacts",
+        permanent: true,
+      },
+      {
+        source: "/ua/terms",
+        destination: "/terms",
+        permanent: true,
+      },
+      {
+        source: "/en/terms",
+        destination: "/terms",
+        permanent: true,
+      },
+      {
+        source: "/ua/privacy",
+        destination: "/privacy",
+        permanent: true,
+      },
+      {
+        source: "/en/privacy",
+        destination: "/privacy",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
